56603777a4d3837a29008fe7e278f0dce74ec59d
Ebbe Vang <ev@evang.dk>
2015-01-13 10:42:12 +0100

    adds project to git
    half way through drag/drop tutorial

---
+++ BattleShips/Cirlce.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BattleShips
+{
+    /// <summary>
+    /// Interaction logic for Cirlce.xaml
+    /// </summary>
+    public partial class Cirlce : UserControl
+    {
+        public Cirlce()
+        {
+            InitializeComponent();
+        }
+        public Cirlce(Cirlce c)
+        {
+            InitializeComponent();
+            this.circleUI.Height = c.circleUI.Height;
+            this.circleUI.Width = c.circleUI.Height;
+            this.circleUI.Fill = c.circleUI.Fill;
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            base.OnGiveFeedback(e);
+            // These Effects values are set in the drop target's 
+            // DragOver event handler. 
+            if (e.Effects.HasFlag(DragDropEffects.Copy))
+            {
+                Mouse.SetCursor(Cursors.Cross);
+            }
+            else if (e.Effects.HasFlag(DragDropEffects.Move))
+            {
+                Mouse.SetCursor(Cursors.Pen);
+            }
+            else
+            {
+                Mouse.SetCursor(Cursors.No);
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // Package the data.
+                DataObject data = new DataObject();
+                data.SetData(DataFormats.StringFormat, circleUI.Fill.ToString());
+                data.SetData("Double", circleUI.Height);
+                data.SetData("Object", this);
+
+                // Inititate the drag-and-drop operation.
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Copy | DragDropEffects.Move);
+            }
+        }
+
+    }
+}
